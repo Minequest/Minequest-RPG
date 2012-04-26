0f1081fc9d9d7393dad4778900bde376d03721b9
Robert Xu <robxu9@gmail.com>
2012-04-25 21:30:39 -0400

    er... no.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/com/theminequest/MQCoreRPG/SpoutPlugin/PopupManager.java
+++ src/main/java/com/theminequest/MQCoreRPG/SpoutPlugin/PopupManager.java
@@ -24,7 +24,7 @@ public class PopupManager implements Listener {
 	
 	public void triggerNotificationPopup(Player player, String message){
 		final SpoutPlayer sp = SpoutManager.getPlayer(player);
-		final Widget text = new GenericLabel(message).setTextColor(new Color(192,192,192)).setHeight(10).setWidth(20).setAnchor(WidgetAnchor.BOTTOM_LEFT).shiftYPos(-10).animate(WidgetAnim.POS_Y, -1F, (short)20, (short)1, true, false).animateStart();
+		final Widget text = new GenericLabel(message).setTextColor(new Color(192,192,192)).setHeight(10).setWidth(20).setAnchor(WidgetAnchor.BOTTOM_LEFT).shiftYPos(-10).animate(WidgetAnim.POS_Y, -1F, (short)20, (short)1, false, false).animateStart();
 		sp.getMainScreen().attachWidget(MQCoreRPG.activePlugin,text);
 		
 		MQCoreRPG.activePlugin.getServer().getScheduler().scheduleSyncDelayedTask(MQCoreRPG.activePlugin, new Runnable() {
@@ -52,7 +52,7 @@ public class PopupManager implements Listener {
 	@EventHandler
 	public void onPlayerLevel(PlayerLevelEvent e){
 		final SpoutPlayer splayer = SpoutManager.getPlayer(e.getPlayer());
-		final Widget levelup = new GenericLabel("Level Up!").setTextColor(new Color(0,255,0)).setAlign(WidgetAnchor.CENTER_CENTER).setHeight(10).setWidth(20).setAnchor(WidgetAnchor.CENTER_CENTER).shiftXPos(-4).shiftYPos(-25).animate(WidgetAnim.POS_Y, -1F, (short)20, (short)1, true, false).animateStart();
+		final Widget levelup = new GenericLabel("Level Up!").setTextColor(new Color(0,255,0)).setAlign(WidgetAnchor.CENTER_CENTER).setHeight(10).setWidth(20).setAnchor(WidgetAnchor.CENTER_CENTER).shiftXPos(-4).shiftYPos(-25).animate(WidgetAnim.POS_Y, -1F, (short)20, (short)1, false, false).animateStart();
 		splayer.getMainScreen().attachWidget(MQCoreRPG.activePlugin, levelup);
 
 		MQCoreRPG.activePlugin.getServer().getScheduler().scheduleSyncDelayedTask(MQCoreRPG.activePlugin, new Runnable() {
