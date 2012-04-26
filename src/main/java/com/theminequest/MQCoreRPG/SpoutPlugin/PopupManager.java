9f877f267d0b8b902d8ad98e9688b255e87dbb6b
Robert Xu <robxu9@gmail.com>
2012-04-25 20:52:00 -0400

    Some nice things.
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/com/theminequest/MQCoreRPG/SpoutPlugin/PopupManager.java
+++ src/main/java/com/theminequest/MQCoreRPG/SpoutPlugin/PopupManager.java
@@ -1,6 +1,7 @@
 package com.theminequest.MQCoreRPG.SpoutPlugin;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.getspout.spoutapi.SpoutManager;
 import org.getspout.spoutapi.gui.Color;
@@ -11,6 +12,7 @@ import org.getspout.spoutapi.gui.WidgetAnim;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 import com.theminequest.MQCoreRPG.MQCoreRPG;
+import com.theminequest.MQCoreRPG.BukkitEvents.PlayerLevelEvent;
 import com.theminequest.MineQuest.MineQuest;
 
 public class PopupManager implements Listener {
@@ -22,7 +24,7 @@ public class PopupManager implements Listener {
 	
 	public void triggerNotificationPopup(Player player, String message){
 		final SpoutPlayer sp = SpoutManager.getPlayer(player);
-		final Widget text = new GenericLabel(message).setTextColor(new Color(192,192,192)).setHeight(10).setWidth(20).setAnchor(WidgetAnchor.BOTTOM_LEFT).shiftYPos(-10).animate(WidgetAnim.POS_Y, -1F, (short)20, (short)1, false, false).animateStart();
+		final Widget text = new GenericLabel(message).setTextColor(new Color(192,192,192)).setHeight(10).setWidth(20).setAnchor(WidgetAnchor.BOTTOM_LEFT).shiftYPos(-10).animate(WidgetAnim.POS_Y, -1F, (short)20, (short)1, true, false).animateStart();
 		sp.getMainScreen().attachWidget(MQCoreRPG.activePlugin,text);
 		
 		MQCoreRPG.activePlugin.getServer().getScheduler().scheduleSyncDelayedTask(MQCoreRPG.activePlugin, new Runnable() {
@@ -45,6 +47,20 @@ public class PopupManager implements Listener {
 		    	splayer.getMainScreen().removeWidget(exp);
 		    }
 		}, 20L);
+	}
+	
+	@EventHandler
+	public void onPlayerLevel(PlayerLevelEvent e){
+		final SpoutPlayer splayer = SpoutManager.getPlayer(e.getPlayer());
+		final Widget levelup = new GenericLabel("Level Up!").setTextColor(new Color(0,255,0)).setAlign(WidgetAnchor.CENTER_CENTER).setHeight(10).setWidth(20).setAnchor(WidgetAnchor.CENTER_CENTER).shiftXPos(-4).shiftYPos(-25).animate(WidgetAnim.POS_Y, -1F, (short)20, (short)1, true, false).animateStart();
+		splayer.getMainScreen().attachWidget(MQCoreRPG.activePlugin, levelup);
+
+		MQCoreRPG.activePlugin.getServer().getScheduler().scheduleSyncDelayedTask(MQCoreRPG.activePlugin, new Runnable() {
+		    public void run() {
+		    	splayer.getMainScreen().removeWidget(levelup);
+		    }
+		}, 200L);
+
 	}
 
 }
