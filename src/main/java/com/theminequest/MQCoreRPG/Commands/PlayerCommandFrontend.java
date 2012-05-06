1b8aabb673032b155b4ac81f2a81f5b386388398
Robert Xu <robxu9@gmail.com>
2012-05-05 21:45:26 -0400

    Fix NPE
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- src/main/java/com/theminequest/MQCoreRPG/Commands/PlayerCommandFrontend.java
+++ src/main/java/com/theminequest/MQCoreRPG/Commands/PlayerCommandFrontend.java
@@ -21,211 +21,215 @@ public class PlayerCommandFrontend extends CommandFrontend {
 	public PlayerCommandFrontend() {
 		super("player");
 	}
-	
+
 	// TODO Implement localization fully.
-	
+
 	public Boolean clear(Player p, String[] args){
 		if (p!=null){
 			if (!p.isOp())
 				return false;
 		}
-		
+
 		CommandSender c;
 		if (p==null)
 			c = Bukkit.getConsoleSender();
 		else
 			c = p;
-		
+
 		if (args.length!=1){
 			c.sendMessage("Invalid number of arguments.");
 			return false;
 		}
-		
+
 		Player player = Bukkit.getPlayerExact(args[0]);
 		if (player==null){
 			c.sendMessage("No such player. :(");
 			return false;
 		}
-		
+
 		PlayerDetails details = MQCoreRPG.playerManager.getPlayerDetails(player);
 		details.modifyExperienceBy((int) -(details.getExperience()));
 		details.setLevel(1);
 		c.sendMessage("Cleared.");
 		return true;
 	}
-	
+
 	public Boolean setlevel(Player p, String[] args){
 		if (p!=null){
 			if (!p.isOp())
 				return false;
 		}
-		
+
 		CommandSender c;
 		if (p==null)
 			c = Bukkit.getConsoleSender();
 		else
 			c = p;
-		
+
 		if (args.length!=2){
 			c.sendMessage("Invalid number of arguments.");
 			return false;
 		}
-		
+
 		Player player = Bukkit.getPlayerExact(args[0]);
 		if (player==null){
 			c.sendMessage("No such player. :(");
 			return false;
 		}
-		
+
 		PlayerDetails details = MQCoreRPG.playerManager.getPlayerDetails(player);
 		details.setLevel(Integer.parseInt(args[1]));
 		c.sendMessage("Modified.");
 		return true;
 	}
-	
+
 	public Boolean setclass(Player p, String[] args){
 		if (p!=null){
 			if (!p.isOp())
 				return false;
 		}
-		
+
 		CommandSender c;
 		if (p==null)
 			c = Bukkit.getConsoleSender();
 		else
 			c = p;
-		
+
 		if (args.length!=2){
 			c.sendMessage("Invalid number of arguments.");
 			return false;
 		}
-		
+
 		Player player = Bukkit.getPlayerExact(args[0]);
 		if (player==null){
 			c.sendMessage("No such player. :(");
 			return false;
 		}
-		
+
 		PlayerDetails details = MQCoreRPG.playerManager.getPlayerDetails(player);
 		details.setClassID(args[1]);
 		c.sendMessage("Modified.");
 		return true;
 	}
-	
+
 	public Boolean giveexp(Player p, String[] args){
 		if (p!=null){
 			if (!p.isOp())
 				return false;
 		}
-		
+
 		CommandSender c;
 		if (p==null)
 			c = Bukkit.getConsoleSender();
 		else
 			c = p;
-		
+
 		if (args.length!=2){
 			c.sendMessage("Invalid number of arguments.");
 			return false;
 		}
-		
+
 		Player player = Bukkit.getPlayerExact(args[0]);
 		if (player==null){
 			c.sendMessage("No such player. :(");
 			return false;
 		}
-		
+
 		PlayerDetails details = MQCoreRPG.playerManager.getPlayerDetails(player);
 		details.modifyExperienceBy((int)Double.parseDouble(args[1]));
 		c.sendMessage("Modified.");
 		return true;
 	}
-	
+
 	public Boolean levelup(Player p, String[] args){
 		if (p!=null){
 			if (!p.isOp())
 				return false;
 		}
-		
+
 		CommandSender c;
 		if (p==null)
 			c = Bukkit.getConsoleSender();
 		else
 			c = p;
-		
+
 		if (args.length!=1){
 			c.sendMessage("Invalid number of arguments.");
 			return false;
 		}
-		
+
 		Player player = Bukkit.getPlayerExact(args[0]);
 		if (player==null){
 			c.sendMessage("No such player. :(");
 			return false;
 		}
-		
+
 		PlayerDetails details = MQCoreRPG.playerManager.getPlayerDetails(player);
 		details.levelUp();
 		c.sendMessage("Leveled.");
 		return true;
 	}
-	
+
 	public Boolean heal(Player p, String[] args){
 		if (p!=null){
 			if (!p.isOp())
 				return false;
 		}
-		
+
 		CommandSender c;
 		if (p==null)
 			c = Bukkit.getConsoleSender();
 		else
 			c = p;
-		
+
 		if (args.length>1){
 			c.sendMessage("Invalid number of arguments.");
 			return false;
 		}
-		
-		Player player = Bukkit.getPlayerExact(args[0]);
+
+		Player player;
+		if (args.length==1)
+			player = Bukkit.getPlayerExact(args[0]);
+		else
+			player = p;
 		if (player==null){
 			c.sendMessage("No such player. :(");
 			return false;
 		}
-		
+
 		PlayerDetails details = MQCoreRPG.playerManager.getPlayerDetails(player);
 		details.setHealth(details.getMaxHealth());
 		c.sendMessage("Healed.");
 		return true;
-		
+
 	}
-	
+
 	public Boolean info(Player p, String[] args) {
 		CommandSender c;
 		if (p==null)
 			c = Bukkit.getConsoleSender();
 		else
 			c = p;
-		
+
 		if (args.length>1){
 			c.sendMessage("Invalid number of arguments.");
 			return false;
 		}
-		
+
 		Player lookup;
-		
+
 		if (args.length==0)
 			lookup = p;
 		else
 			lookup = Bukkit.getPlayerExact(args[0]);
-		
+
 		if (lookup==null){
 			c.sendMessage("No such player.");
 			return false;
 		}
-		
+
 		PlayerDetails details = MQCoreRPG.playerManager.getPlayerDetails(lookup);
-		
+
 		List<String> messages = new ArrayList<String>();
 		messages.add(ChatUtils.formatHeader("Player Information: " + lookup.getName()));
 		messages.add(ChatColor.AQUA + "Display Name: " + ChatColor.WHITE + lookup.getDisplayName());
@@ -236,7 +240,7 @@ public class PlayerCommandFrontend extends CommandFrontend {
 			messages.add(ChatColor.AQUA + "Exp: " + ChatColor.WHITE + details.getExperience() + "/" + (details.getMaxExperience()));
 			messages.add(ChatColor.AQUA + "Power: " + ChatColor.WHITE + details.getPower() + "/" + (details.getMaxPower()));
 		}
-		
+
 		for (String s: messages){
 			c.sendMessage(s);
 		}
@@ -246,7 +250,7 @@ public class PlayerCommandFrontend extends CommandFrontend {
 	@Override
 	public Boolean help(Player p, String[] args) {
 		List<String> messages = new ArrayList<String>();
-		
+
 		// CONSOLE COMMANDS
 		if (p==null || p.isOp()){
 			messages.add(ChatUtils.formatHeader("Op Commands"));
